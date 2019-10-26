1d5a50c1b489840e53ce6aa4638d0413ac66d9e2
Fiko Borizqy <fikokuper@gmail.com>
2019-10-26 15:00:53 +0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# fikoborizqy.github.io
-Personal web
---
+++ precache-manifest.a3b24f83bfb7028d86de01fb91dc7bd2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afb82fa6e62f3c8e4ea50df99d667fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94741c4918ec677fa7ad",
+    "url": "/static/css/main.8046f74d.chunk.css"
+  },
+  {
+    "revision": "677d26f9e63ec79c5bc0",
+    "url": "/static/js/2.6cf45907.chunk.js"
+  },
+  {
+    "revision": "94741c4918ec677fa7ad",
+    "url": "/static/js/main.4160a8ce.chunk.js"
+  },
+  {
+    "revision": "acded484ad2ad781ca43",
+    "url": "/static/js/runtime-main.50e3c7ab.js"
+  },
+  {
+    "revision": "38d42861e72a515ee77183854748ab96",
+    "url": "/static/media/logo-github.38d42861.svg"
+  },
+  {
+    "revision": "018f55b6c2e679707852a1a511a1c3d6",
+    "url": "/static/media/logo-linkedin.018f55b6.svg"
+  },
+  {
+    "revision": "76438be9e6c27a15e48a32a523194ddc",
+    "url": "/static/media/logo-medium.76438be9.svg"
+  },
+  {
+    "revision": "6e4e24020c99e1cacb591dcd952e8411",
+    "url": "/static/media/me_240.6e4e2402.jpg"
+  }
+]);
